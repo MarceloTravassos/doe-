951034b141a171e067a44fa0c15d526b9590ee48
Marcelo Sarinho <marcelo.sarinho@fatec.sp.gov.br>
2023-03-08 20:25:29 -0300

    added mobile home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Navbar } from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Navbar />
+      <main>
+        <div className='flex flex-col items-center justify-center text-center'>
+          <h1 className='font-bold text-5xl mb-8'>Texto chamativo aqui</h1>
+          <p className='text-lg mb-8 px-10'>Texto complementar explicando o que o aplicativo faz</p>
+          <button className='bg-black text-white px-10 py-1 rounded font-bold'>Iniciar</button>
+        </div>
+      </main>
+    </>
+    );
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Navbar () {
+    return (
+        <>
+				<nav className="h-14 bg-zinc-300 flex items-center mb-4">
+					<h1 className="font-bold text-lg ml-4">Doe+</h1>
+				</nav>
+        </>
+    )
+}
